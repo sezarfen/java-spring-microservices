cd122ed745ecb4467579eec8dcc5257e92ab64f7
furkandundar <furkandndr2014@gmail.com>
2026-08-26 12:51:38 +0300

    KafkaProducer set inside of patient-service

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -6,6 +6,7 @@ import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.exception.EmailAlreadyExistsException;
 import com.pm.patientservice.exception.PatientNotFoundException;
 import com.pm.patientservice.grpc.BillingServiceGrpcClient;
+import com.pm.patientservice.kafka.KafkaProducer;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
@@ -23,13 +24,16 @@ public class PatientService {
 
     private static final Logger log = LoggerFactory.getLogger(PatientService.class);
     private final PatientRepository patientRepository;
+    private final KafkaProducer kafkaProducer;
 
     private final BillingServiceGrpcClient billingServiceGrpcClient;
 
     public PatientService(PatientRepository patientRepository,
-                          BillingServiceGrpcClient billingServiceGrpcClient){
+                          BillingServiceGrpcClient billingServiceGrpcClient,
+                          KafkaProducer kafkaProducer){
         this.patientRepository = patientRepository;
         this.billingServiceGrpcClient = billingServiceGrpcClient;
+        this.kafkaProducer = kafkaProducer;
     }
 
     public List<PatientResponseDTO> getPatients(){
@@ -53,6 +57,8 @@ public class PatientService {
                 newPatient.getName(),
                 newPatient.getEmail(),
                 "ACTIVE");
+
+        kafkaProducer.sendEvent(newPatient);
 
         log.info("BillingResponse received inside PatientService: {}", billingResponse);
 
