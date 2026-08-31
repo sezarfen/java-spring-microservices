49cd96e982866592f8de04c5cf38aca35005ba87
furkandundar <furkandndr2014@gmail.com>
2026-08-31 20:40:12 +0300

    KafkaConsumer build for analytics service

--- analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
+++ analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
@@ -1,13 +1,31 @@
 package com.pm.analyticsservice.kafka;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
 
 @Service
 public class KafkaConsumer {
 
+    private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    // groupId consumer'ın kim olduğunu söylüyor, biraz daha advanced bir topic şu an için.
     @KafkaListener(topics = "patient", groupId = "analytics-service")
     public void consumeEvent(byte[] event) {
+        try {
 
+            PatientEvent patientEvent = PatientEvent.parseFrom(event);
+            // .. analitik servis için bir business kodu yazabiliriz buraya
+            log.info("Received Patient Event: [PatientId={}, PatientName={}, PatientEmail={}]",
+                    patientEvent.getPatientId(), patientEvent.getName(), patientEvent.getEmail());
+        } catch (InvalidProtocolBufferException e) {
+
+            log.error("Error Deserializing event {}", e.getMessage());
+            // Exception fırlatıp analitik servisi durdurmadan loglayalım
+
+        }
     }
 }
