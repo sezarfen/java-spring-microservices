4f571214a1ace8b4033a4291fbb2648502a51c9e
furkandundar <furkandndr2014@gmail.com>
2026-09-04 12:02:43 +0300

    auth-service initiated

---
+++ auth-service/src/main/java/com/pm/authservice/model/User.java
@@ -0,0 +1,22 @@
+package com.pm.authservice.model;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
