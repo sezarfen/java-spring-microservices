d5ca5419bdf84a9542b60031b5d6eeaec0e569eb
furkandundar <furkandndr2014@gmail.com>
2026-09-04 11:16:41 +0300

    api-gateway implemented, preventing outcoming requests to communicate directly with interval services

---
+++ api-gateway/src/main/java/com/pm/apigateway/ApiGateawayApplication.java
@@ -0,0 +1,13 @@
+package com.pm.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateawayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGateawayApplication.class, args);
+    }
+
+}
